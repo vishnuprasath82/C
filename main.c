b9b0c87e1090eb92f268e9b6dfb5aeb80b1a9bcf
vishnuprasath82 <prasathv633@gmail.com>
2025-05-08 10:18:37 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,10 @@
+/*Hello i am vishnu 3rd biomedicl engineering in shanmugha college of engineering and technology .To create a hello world program to use the  'c' language.***/
+#include <stdio.h>
+int main(){
+    printf("%d",1954-1235);
+    
+    return 0;
+}
+
+
+
